bfb0efdcd8f417c6dba40f0ac29d0b84a423f8d9
Alexandra <alexbushueva27@gmail.com>
2021-07-06 23:25:25 +0300

    Add hardcoded tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import './App.css';
 import {Todolist} from "./components/todolist/Todolist";
 
 function App() {
+    const tasks1 = [
+        {id: 1, title: 'Go to the beach', isDone: true},
+        {id: 2, title: 'Learn redux-toolkit', isDone: false},
+        {id: 3, title: 'Codewars', isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: 'Hello world', isDone: true},
+        {id: 2, title: 'I am happy', isDone: true},
+        {id: 3, title: 'Yo!', isDone: true}
+    ]
+
   return (
     <div className="App">
-      <Todolist title={'What to do?'} />
-      <Todolist title={'What to buy?'} />
-      <Todolist title={'What to learn?'} />
+      <Todolist title={'What to do?'} tasks={tasks1} />
+      <Todolist title={'Hello world'} tasks={tasks2} />
+
     </div>
   );
 }
--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import s from './Todolist.module.css'
 
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 type PropsType = {
     title: string
+    tasks: Array<TaskType>
 }
 
 export const Todolist = (props: PropsType) => {
@@ -11,10 +17,10 @@ export const Todolist = (props: PropsType) => {
             <h3>{props.title}</h3>
             <input type={'text'}/>
             <button>Add</button>
-            <div>
-                <input type={'checkbox'} checked={true}/>
-                <span>Go to the beach</span>
-            </div>
+            {props.tasks.map(el => <div key={el.id}>
+                <input type={'checkbox'} checked={el.isDone}/>
+                <span>{el.title}</span>
+            </div>)}
 
 
         </div>
